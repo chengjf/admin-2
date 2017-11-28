db911ae04d0015d944eaa2e3119a25094aa27ebc
chengjf <sharpkingdom@gmail.com>
2017-11-29 01:01:53 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,33 @@
 <template>
   <div id="app">
-   <b-alert show>Default Alert</b-alert>
-    <b-container class="bv-example-row">
-		<b-row align-v="start">
-			<b-col>HEAD</b-col>
-			<b-nav pills>
-  <b-nav-item active>Active</b-nav-item>
-  <b-nav-item>Link</b-nav-item>
-  <b-nav-item-dropdown id="nav7_ddown" text="Dropdown" right>
-    <b-dropdown-item>one</b-dropdown-item>
-    <b-dropdown-item>two</b-dropdown-item>
-    <b-dropdown-divider></b-dropdown-divider>
-    <b-dropdown-item>three</b-dropdown-item>
-  </b-nav-item-dropdown>
-</b-nav>
-		</b-row>
-		<b-row align-v="center">
+    <b-container class="bv-example-row" style="min-height:100%">
+	<b-row>
+		<b-col class="w-25">
+			<b-nav vertical class="w-25">
+				<b-nav-item active>Active</b-nav-item>
+				<b-nav-item>Link</b-nav-item>
+				<b-nav-item>Another Link</b-nav-item>
+				<b-nav-item disabled>Disabled</b-nav-item>
+			</b-nav>
+		</b-col>
+		<b-col class="w-75">
+			<b-row align-v="start">
+				<b-col>HEAD</b-col>
+			</b-row>
+			<b-row align-v="center">
 			<b-col>
-				<b-nav vertical class="w-50">
-				  <b-nav-item active>Active</b-nav-item>
-				  <b-nav-item>Link</b-nav-item>
-				  <b-nav-item>Another Link</b-nav-item>
-				  <b-nav-item disabled>Disabled</b-nav-item>
-				</b-nav>
+				
 			</b-col>
 			<b-col>One of three columns</b-col>
 			
-		</b-row>
-		<b-row align-v="end">
-			<b-col>FOOTER</b-col>
-		</b-row>
+			</b-row>
+			<b-row align-v="end">
+				<b-col>FOOTER</b-col>
+			</b-row>
+			<footer>COPYRIGHT</footer>
+		</b-col>
+	</b-row>
+	
 	</b-container>
   </div>
 </template>
